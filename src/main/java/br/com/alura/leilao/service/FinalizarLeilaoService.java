7157fba49b556da461d8bd7562ea4a01c30ce189
RobsonAlmeida <robsonalmeida073@gmail.com>
2021-12-28 17:00:59 -0300

    Escrevendo um teste com Mockito

--- src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
+++ src/main/java/br/com/alura/leilao/service/FinalizarLeilaoService.java
@@ -12,25 +12,29 @@ import br.com.alura.leilao.model.Leilao;
 @Service
 public class FinalizarLeilaoService {
 
-	@Autowired
-	private LeilaoDao leiloes;
+    private LeilaoDao leiloes;
 
-	public void finalizarLeiloesExpirados() {
-		List<Leilao> expirados = leiloes.buscarLeiloesExpirados();
-		expirados.forEach(leilao -> {
-			Lance maiorLance = maiorLanceDadoNoLeilao(leilao);
-			leilao.setLanceVencedor(maiorLance);
-			leilao.fechar();
-			leiloes.salvar(leilao);
-		});
-	}
+    @Autowired
+    public FinalizarLeilaoService(LeilaoDao leiloes) {
+        this.leiloes = leiloes;
+    }
 
-	private Lance maiorLanceDadoNoLeilao(Leilao leilao) {
-		List<Lance> lancesDoLeilao = leilao.getLances();
-		lancesDoLeilao.sort((lance1, lance2) -> {
-			return lance2.getValor().compareTo(lance1.getValor());
-		});
-		return lancesDoLeilao.get(0);
-	}
-	
+    public void finalizarLeiloesExpirados() {
+        List<Leilao> expirados = leiloes.buscarLeiloesExpirados();
+        expirados.forEach(leilao -> {
+            Lance maiorLance = maiorLanceDadoNoLeilao(leilao);
+            leilao.setLanceVencedor(maiorLance);
+            leilao.fechar();
+            leiloes.salvar(leilao);
+        });
+    }
+
+    private Lance maiorLanceDadoNoLeilao(Leilao leilao) {
+        List<Lance> lancesDoLeilao = leilao.getLances();
+        lancesDoLeilao.sort((lance1, lance2) -> {
+            return lance2.getValor().compareTo(lance1.getValor());
+        });
+        return lancesDoLeilao.get(0);
+    }
+
 }
---
+++ src/test/java/br/com/alura/leilao/service/FinalizarLeilaoServiceTest.java
@@ -0,0 +1,64 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.alura.leilao.service;
+
+import br.com.alura.leilao.dao.LeilaoDao;
+import br.com.alura.leilao.model.Lance;
+import br.com.alura.leilao.model.Leilao;
+import br.com.alura.leilao.model.Usuario;
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Test;
+import org.junit.jupiter.api.BeforeEach;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+/**
+ *
+ * @author 99030499
+ */
+public class FinalizarLeilaoServiceTest {
+
+    private FinalizarLeilaoService service;
+
+    @Mock
+    private LeilaoDao leilaoDao;
+
+    @BeforeEach
+    public void setUp() {
+        MockitoAnnotations.initMocks(this);
+        this.service = new FinalizarLeilaoService(leilaoDao);
+    }
+
+    @Test
+    public void testFinalizarLeilao() {
+        List<Leilao> leiloes = leiloes();
+        service.finalizarLeiloesExpirados();
+
+    }
+    
+    private List<Leilao> leiloes(){
+        List<Leilao> lista = new ArrayList<>();
+        
+        Leilao leilao = new Leilao("Celular"
+                , new BigDecimal("500")
+                , new Usuario("fulano"));
+        
+        Lance primeiro = new Lance(new Usuario("Beltrano"),
+                new BigDecimal("600"));
+        Lance segundo = new Lance(new Usuario("CiClano"),
+                new BigDecimal("900"));
+        
+        leilao.propoe(primeiro);
+        leilao.propoe(segundo);
+        
+        lista.add(leilao);
+        
+        return lista;
+    }
+
+}
